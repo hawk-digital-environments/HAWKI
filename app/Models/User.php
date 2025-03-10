62dcd161bd7e264317cad6505869f518f10f174f
Ariansdf <38947447+Ariansdf@users.noreply.github.com>
2025-03-10 13:04:20 +0100

    Orchid Base

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Orchid\Filters\Types\Like;
+use Orchid\Filters\Types\Where;
+use Orchid\Filters\Types\WhereDateStartEnd;
+use Orchid\Platform\Models\User as OrchidUser;
 
-class User extends Authenticatable
+class User extends OrchidUser
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name',
         'email',
@@ -20,22 +22,70 @@ class User extends Authenticatable
         'publicKey',
         'avatar_id',
         'bio',
-        'isRemoved'
     ];
 
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'permissions',
+    ];
 
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'permissions' => 'array',
+    ];
+
+    /**
+     * The attributes for which you can use filters in url.
+     *
+     * @var array
+     */
+    protected $allowedFilters = [
+        'id' => Where::class,
+        'name' => Like::class,
+        'email' => Like::class,
+        'updated_at' => WhereDateStartEnd::class,
+        'created_at' => WhereDateStartEnd::class,
+    ];
+
+    /**
+     * The attributes for which can use sort in url.
+     *
+     * @var array
+     */
+    protected $allowedSorts = [
+        'id',
+        'name',
+        'email',
+        'updated_at',
+        'created_at',
+    ];
+
+    // Your existing relationships like members, rooms etc.
     public function members()
     {
-        return $this->hasMany(Member::class)->where('isRemoved', false);
+        return $this->hasMany(Member::class);
     }
 
     public function rooms()
     {
-        return $this->belongsToMany(Room::class, 'members', 'user_id', 'room_id')
-                    ->wherePivot('isRemoved', false);
+        return $this->hasManyThrough(
+            Room::class,
+            Member::class,
+            'user_id',
+            'id',
+            'id',
+            'room_id'
+        );
     }
 
-    // Define the relationship with AiConv
     public function conversations()
     {
         return $this->hasMany(AiConv::class);
@@ -45,9 +95,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Invitation::class, 'username', 'username');
     }
-
-    public function revokProfile(){
-        $this->update(['isRemoved'=> 1]);
-    }
-
 }
