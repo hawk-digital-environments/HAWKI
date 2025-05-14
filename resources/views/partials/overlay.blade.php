5c689b0245ce4b6d9a8cb66221a2fc293070e718
Sten Seegel <sten.seegel@gmail.com>
2025-05-14 09:19:34 +0200

    fix missing </div> in overlay.blade.php

--- resources/views/partials/overlay.blade.php
+++ resources/views/partials/overlay.blade.php
@@ -2,7 +2,7 @@
 <div id="overlay"
 
 @if($activeOverlay)
-    style =" visibility: visible; opacity: 1;"
+    style ="visibility: visible; opacity: 1;"
 @else
     style ="visibility: hidden; opacity: 0;"
 @endif
@@ -11,6 +11,7 @@
         <div id="overlay-logo">
            {{--  <img src="{{ asset('img/logo.png') }}" alt="">  --}}
            <img src="{{ route('system.image', 'logo_svg') }}" alt="Logo" style="height: 15vh;">
+        </div>
     </div>
 </div>
 
