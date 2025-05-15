b095d72878021a15532b4ba003b265b29e7cd396
Raphael Fetzer <fetzer@vcrp.de>
2025-05-15 16:12:02 +0200

    Revert "Check if at least one member is added"
    This reverts commit d06b3560adcbe62bab61d2a072cd7fb8cd09fd9f

--- public/js_v2.0.0/groupchat_functions.js
+++ public/js_v2.0.0/groupchat_functions.js
@@ -529,13 +529,6 @@ async function sendInvitation(btn){
             listOfInvitees.push(userObj);
         }
     });
-
-    // Check if no invitees selected
-    if (listOfInvitees.length === 0) {
-        alert(translation.Cnf_checkMembersAdded);
-        return;
-    }
-
     await createAndSendInvitations(listOfInvitees, activeRoom.slug);
     closeModal(btn);
 }
