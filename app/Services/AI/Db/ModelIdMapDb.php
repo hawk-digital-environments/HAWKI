6dc7d98fb429fef327de261d7e51c0cc0a79fa50
neunerlei <ich@neunerlei.eu>
2025-12-05 00:33:02 +0100

    fix: fix weird issue when generating ai model map

--- app/Services/AI/Db/ModelIdMapDb.php
+++ app/Services/AI/Db/ModelIdMapDb.php
@@ -12,7 +12,7 @@ use App\Services\AI\Value\AiModel;
 class ModelIdMapDb
 {
     private ?array $modelIdMap = null;
-    
+
     /**
      * Get the numeric ID for a given model ID, or assign a new one if it doesn't exist.
      * The frontend sync uses numeric IDs for efficiency, therefore we need to map the model id string to a numeric ID.
@@ -23,20 +23,20 @@ class ModelIdMapDb
     public function getOrAssignNumericId(AiModel $model): int
     {
         $this->populateMap();
-        
+
         $modelId = $model->getId();
         if (isset($this->modelIdMap[$modelId])) {
             return $this->modelIdMap[$modelId];
         }
-        
-        $newId = AiModelIdMap::create(['model_id' => $modelId])->id;
+
+        $newId = AiModelIdMap::createOrFirst(['model_id' => $modelId])->id;
         $this->modelIdMap[$modelId] = $newId;
-        
+
         AiModelNumericIdAssignedEvent::dispatch($model);
-        
+
         return $newId;
     }
-    
+
     /**
      * Get the model ID string for a given numeric ID.
      *
@@ -46,17 +46,16 @@ class ModelIdMapDb
     public function getModelIdByNumeric(int $id): ?string
     {
         $this->populateMap();
-        
+
         $flippedMap = array_flip($this->modelIdMap);
         return $flippedMap[$id] ?? null;
     }
-    
+
     private function populateMap(): void
     {
         if ($this->modelIdMap !== null) {
             return;
         }
-        
         $this->modelIdMap = [];
         foreach (AiModelIdMap::query()->get(['id', 'model_id']) as $row) {
             $this->modelIdMap[$row->model_id] = $row->id;
