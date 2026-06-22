31dd112e64d19c86a30ef456a8cbfdbb10c17be9
neunerlei <ich@neunerlei.eu>
2026-06-22 08:03:26 +0200

    tests: remove no longer required test

--- tests/Unit/Services/System/JsonApi/Eloquent/ScopedSchema/ScopedSchemaServerTraitTestFixtures/ValidModelFixture.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Tests\Unit\Services\System\JsonApi\Eloquent\ScopedSchema\ScopedSchemaServerTraitTestFixtures;
-
-use Illuminate\Database\Eloquent\Model;
-
-class ValidModelFixture extends Model {}
